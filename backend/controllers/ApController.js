9215eb2061a366d30c620c838ef9f79843e2163a
Antônio Pedro <antoniopedro.rs9@gmail.com>
2022-05-12 23:51:19 -0300

    AP controller refactored

--- backend/controllers/ApController.js
+++ backend/controllers/ApController.js
@@ -1,61 +1,80 @@
-const express = require("express");
 const fs = require("fs");
-const exec = require("child_process").exec;
-
-const json2yaml =
-  "json2yaml ../ansible/json/vars.json > ../ansible/yml/vars.yml && ansible-playbook -i ../ansible/yml/hosts ../ansible/yml/create_ap.yml"; //converte JSON->YAML & EXECUTA COMANDO ANSIBLE
+const { exec } = require("child_process");
 
 class AP {
+  /**
+   * /aps:
+   *   post:
+   *     description: Usada para solicitar a criação de um AP
+   *     responses:
+   *       '200':
+   *         description: Solicitação feita com sucesso
+   *       '400':
+   *         description: Falha na solicitação
+   */
   async create(request, response) {
     try {
-      const { ApParam } = request.body;
-      if (ApParam) {
-        if (!ApParam.ap || !ApParam.description || !ApParam.tenant) {
-          throw "AP Name, description or Tenant on ApParam does not exists";
-        }
-        fs.writeFileSync("./ansible/json/vars.json", JSON.stringify(ApParam, undefined, 2));
+      const { data } = request.body;
 
-        await exec(json2yaml, { cwd: __dirname }, (err, stdout, stderr) => {
-          if (err) {
-            const merged = { err, stdout };
-            return response.status(400).json({ createdAP: false, error: merged });
-          } else {
-            runCommand(cmds, cb);
-            return response.status(200).json({ createdAP: true, statusMessage: "AP created successfully." });
-          }
-        });
-      } else {
-        throw "ApParam parameter does not exists";
-      }
+      if (!data) throw "AP data was not received.";
+      if (!data.name) throw "AP name is missing.";
+      if (!data.description) throw "AP description is missing.";
+      if (!data.tenant) throw "AP tenant is missing.";
+
+      /**
+       * Escreve as informações do tenant no arquivo "vars.json"
+       */
+      fs.writeFileSync("./ansible/json/vars.json", JSON.stringify(data, undefined, 2));
+
+      const json2yaml =
+        "json2yaml ../ansible/json/vars.json > ../ansible/yml/vars.yml && ansible-playbook -i ../ansible/yml/hosts ../ansible/yml/create_ap.yml"; //converte JSON->YAML & EXECUTA COMANDO ANSIBLE
+
+      /**
+       * Executa o comando para criar um tenant na máquina
+       */
+      await exec(json2yaml, { cwd: __dirname }, (error, stdout, stderr) => {
+        if (error) return response.status(400).json({ error, stdout, stderr });
+
+        runCommand(cmds, cb);
+
+        return response.status(200).json({ stdout });
+      });
     } catch (error) {
-      return response.status(400).json({ createdAP: false, error: error });
+      return response.status(400).json({ error });
     }
   }
 
+  /**
+   * /aps:
+   *   get:
+   *     description: Usada para listar os APs
+   *     responses:
+   *       '200':
+   *         description: Listagem feita com sucesso
+   *       '400':
+   *         description: Falha na listagem
+   */
   async index(request, response) {
     try {
-      const queryvrf = fs.readFileSync("./ansible/querys/aci_aps.json"); //le o arquivo
-      const queryvrf_vars = JSON.parse(queryvrf);
+      const queryvrf = fs.readFileSync("./ansible/querys/aci_aps.json"); // Lê o arquivo
+      const queryvrf_vars = JSON.parse(queryvrf); // Converte o arquivo para JSON
 
       var names = [];
+      var containerId; // Variável de controle para não pegar o mesmo ID
 
-      // variável de controle para não pegar o mesmo id
-      var containerId;
       for (let i in queryvrf_vars.current[0].fvTenant.children) {
-        // atribui o valor de containerId a variável id
-        let id = queryvrf_vars.current[0].fvTenant.children[i].fvAp.attributes.name;
-        // se for diferente, pega o valor de name
+        let id = queryvrf_vars.current[0].fvTenant.children[i].fvAp.attributes.name; // Atribui o valor de containerId a variável ID
+
+        // Se for diferente, pega o valor de name
         if (containerId != id) {
-          // redefine o valor da variável com o valor atual
-          containerId = id;
-          // adiciona as names à array
+          containerId = id; // Redefine o valor da variável com o valor atual
           names.push(queryvrf_vars.current[0].fvTenant.children[i].fvAp.attributes.name);
         }
       }
       const queryvrf_formatted = names.map((c) => ({
         label: c,
         value: c,
-      })); //QUERY VRFS ON TENANT FIM
+      }));
 
       return response.status(200).json({ showAp: true, aps: queryvrf_formatted });
     } catch (err) {
